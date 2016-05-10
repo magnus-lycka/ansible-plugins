0136643b423f62b2e6f9adc8ed62d7ef20e5fd55
Magnus Lyckå <magnus@thinkware.se>
2016-05-10 17:05:28 +0200

    Create gocd_resources.py

---
+++ lookup_plugins/gocd_resources.py
@@ -0,0 +1,8 @@
+class LookupModule(object):
+
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, inject=None, **kwargs):
+
+        return []
